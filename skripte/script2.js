daf5c789d643e62019df6662b92a35f3e153976e
Tetka95 <martina.bobek95@gmail.com>
2024-04-10 18:58:41 +0200

    učitavanje script2.js u index.html

--- skripte/script2.js
+++ skripte/script2.js
@@ -1,16 +1,19 @@
 //var firstName = "Marko"; (određena vrijednost varijable firstName kao "Marko")
-//var firstName, lastName, price, discount, fullPrice; ovako se uglavnom ne piše nego onako kako je napisano u nastavku
+var firstName, lastName, price, discount, fullPrice; //(ovako se uglavnom ne piše nego stavljamo var ispred svakog od tagova npr. var firstName; var lastName...)
 
-var firstName = "John";
+firstName = "John";
 var lastName = "Doe"
 
-var price = 19.9;
-var discount = 0.1;
+price = 19.9;
+discount = 0.1;
 
-if (x == 1)
-x = 2
+var x = 1;
+if (x == 1) {
+    x = 2;
+    console.log("x= " + x); 
+}
 
-    if (a) {
+if (a) {
     var z = 44
         return 0;
     }
