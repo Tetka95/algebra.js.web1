3b082ca7a1960dd6332ae1516174ea82ed089307
Tetka95 <martina.bobek95@gmail.com>
2024-04-10 19:18:27 +0200

    testiranje

--- skripte/script3.js
+++ skripte/script3.js
@@ -4,11 +4,22 @@ var zbroj;
 x = 1;
 y = 2;
 
-if (x > 0 && y > 0) {
+/*if (x > 0 && y > 0) {
     zbroj = x + y;
     console.log("Zbroj=" + zbroj);
 } else {
     alert("Unio si negativne vrijednosti!")
+}*/
+
+if (x > 0) {
+    if (y > 0) {
+        zbroj = x + y;
+        console.log("Zbroj=" + zbroj)
+    } else {
+        alert("Unio si negativan y")
+    }
+} else {
+    alert("Unio si negativan x")
 }
 
 //očekujemo zbroj
