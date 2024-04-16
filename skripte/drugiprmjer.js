09c333b60f1241f6b9b8f69bc49f56dcab10d89d
Tetka95 <martina.bobek95@gmail.com>
2024-04-16 18:02:16 +0200

    druga vježba

---
+++ skripte/drugiprmjer.js
@@ -0,0 +1,17 @@
+/**
+// Inicijalizacija varijabli?
+*/
+var first_name = "Tony";
+
+var firstName = "John", 
+lastName = "Doe", 
+price = 0, 
+discount = 78, 
+fullPrice = 0,
+//break = [],
+$myObject = {};
+
+console.log(price);
+console.log(lastName);
+
+// Kraj mog programa;
