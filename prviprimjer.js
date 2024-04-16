f61038a8120c34ebae1e253d8483dde9746b45c8
Tetka95 <martina.bobek95@gmail.com>
2024-04-16 17:16:32 +0200

    primjer JS

---
+++ prviprimjer.js
@@ -0,0 +1,12 @@
+var x = false;
+if (x) 
+    console.log("asdf");
+    console.log("abcd");
+    //izvršit će se samo drugi console.log jer nemamo zagrade
+
+var x = true;
+if (x) {
+    console.log("adsf");
+    console.log("abcd");
+}
+//izvršit će se oba jer su u istoj zagradi
