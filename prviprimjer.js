3031468e770ce1d8263e90240c5ecc14888ff12c
Tetka95 <martina.bobek95@gmail.com>
2024-04-16 18:17:09 +0200

    *prettier

--- prviprimjer.js
+++ prviprimjer.js
@@ -1,13 +1,12 @@
 var x = false;
-if (x) 
-    console.log("asdf");
-    console.log("abcd");
-    //izvršit će se samo drugi console.log jer nemamo zagrade
+if (x) console.log("asdf");
+console.log("abcd");
+//izvršit će se samo drugi console.log jer nemamo zagrade
 
 var x = true;
 if (x == true) {
-    console.log("adsf");
-    console.log("abcd");
+  console.log("adsf");
+  console.log("abcd");
 }
 //izvršit će se oba jer su u istoj zagradi
 //ako napišemo samo (x) onda je x određen kao var x = "bilo koja vrijednost", dodavanjem == određujemo mu vrijednost
