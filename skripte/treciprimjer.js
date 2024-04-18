3cb729a28867737747f866cb533dec7755917395
tetka95 <martina.bobek95@gmail.com>
2024-04-18 17:07:22 +0200

    treći zadatak

---
+++ skripte/treciprimjer.js
@@ -0,0 +1,14 @@
+var x = "Lorem ipsum",
+    y = 2345,
+    z = "2345",
+    q = false,
+    w = null;
+
+x += '3' + 1;
+var baaa = "b" + "a" + +"a" + "a";
+
+console.log(baaa);
+
+console.log(typeof w);
+
+console.log(x);
