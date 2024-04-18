cdfa72370866051197a67321f26c0d2d84b21a05
tetka95 <martina.bobek95@gmail.com>
2024-04-18 17:18:12 +0200

    prettier

--- skripte/treciprimjer.js
+++ skripte/treciprimjer.js
@@ -1,11 +1,11 @@
-var x = "Lorem ipsum",
+var x = 'Lorem ipsum',
     y = 2345,
-    z = "2345",
+    z = '2345',
     q = false,
     w = null;
 
 x += '3' + 1;
-var baaa = "b" + "a" + +"a" + "a";
+var baaa = 'b' + 'a' + +'a' + 'a';
 
 console.log(baaa);
 
