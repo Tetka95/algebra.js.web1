bbf88d7864b9216c57e28e1f46ef713e159f21f3
tetka95 <martina.bobek95@gmail.com>
2024-04-18 19:07:45 +0200

    sedmi primjer

---
+++ skripte/sedmiprimjer.js
@@ -0,0 +1,17 @@
+var x = 'Lorem ipsum',
+    y = 2345,
+    z = '2345',
+    q = false,
+    w = true;
+
+var trueFalse = q;
+
+console.log('trueFalse = ' + trueFalse);
+
+if (trueFalse === 'true') {
+    console.log('1.q = ' + trueFalse);
+}
+
+if (trueFalse !== 'true') {
+    console.log('2.q = ' + trueFalse);
+}
