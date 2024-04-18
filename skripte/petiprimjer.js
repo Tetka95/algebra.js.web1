94a8e9c4a4bce4d73e32a04d28d4ed479cd90e97
tetka95 <martina.bobek95@gmail.com>
2024-04-18 17:51:30 +0200

    peti primjer

---
+++ skripte/petiprimjer.js
@@ -0,0 +1,5 @@
+var myString = 'Lorem ipsum dolor sit amet';
+console.log(myString);
+var duljina = myString.length;
+console.log('Duljina teksta je: ' + myString.length + ' znakova');
+console.log(typeof duljina);
