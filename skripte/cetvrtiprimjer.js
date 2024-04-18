0e7bbe4342c4621c945ce4ae3707e2f39d985dc4
tetka95 <martina.bobek95@gmail.com>
2024-04-18 17:27:54 +0200

    zadatak4

---
+++ skripte/cetvrtiprimjer.js
@@ -0,0 +1,11 @@
+var myString = 'ovo je moj \'string\'';
+
+console.log(myString);
+
+var myString2 = 'ovo je moj \nnovi \'string\'';
+
+console.log(myString2);
+
+var myString3 = 'ovo je moj \\\'string\'';
+
+console.log(myString3);
