aaf32ab1868a32960059eb3d16cf265f2cd7a9f9
tetka95 <martina.bobek95@gmail.com>
2024-04-18 19:17:17 +0200

    && ||

--- skripte/sedmiprimjer.js
+++ skripte/sedmiprimjer.js
@@ -15,3 +15,27 @@ if (trueFalse === 'true') {
 if (trueFalse !== 'true') {
     console.log('2.q = ' + trueFalse);
 }
+
+if (q && w) {
+    console.log('q ili w su true');
+} else {
+    console.log('jedan od q ili w nije true');
+}
+
+console.log('ostatak pri cjelobrojnom dijelnjenju od 9/2 =' + (9 % 2)); 
+console.log('ostatak pri cjelobrojnom dijelnjenju od 5/2 =' + (5 % 2)); 
+
+
+///i jedan i drugi
+if (9 % 3 === 0 && 5 % 2 === 0) {
+    console.log('ostatak pri cjelobrojnom dijelnjenju je 0');
+} else {
+    console.log('ostatak pri cjelobrojnom dijeljenju NIJE 0');
+}
+
+///ili jedan ili drugi
+if (9 % 3 === 0 || 5 % 2 === 0) {
+    console.log('ostatak pri cjelobrojnom dijelnjenju je 0');
+} else {
+    console.log('ostatak pri cjelobrojnom dijeljenju NIJE 0');
+}
