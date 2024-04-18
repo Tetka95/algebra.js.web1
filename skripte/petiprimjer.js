839168c1c583914d3f742c493d01c88c108ea837
tetka95 <martina.bobek95@gmail.com>
2024-04-18 18:15:48 +0200

    peti primjer

--- skripte/petiprimjer.js
+++ skripte/petiprimjer.js
@@ -1,5 +1,18 @@
-var myString = 'Lorem ipsum dolor sit amet';
+var myString = '   Lorem ipsum dolor sit amet ipsum  ';
 console.log(myString);
 var duljina = myString.length;
 console.log('Duljina teksta je: ' + myString.length + ' znakova');
 console.log(typeof duljina);
+console.log('Znak na mjestu 12 je: ' + myString.charAt(12));
+console.log(myString.trim());
+console.log(myString.trimStart());
+console.log(myString.trimEnd());
+console.log(myString.indexOf('ipsum'));
+console.log(myString.replace('ipsum', 'novi string'));
+console.log(myString.replaceAll('ipsum', 'novi string'));
+console.log(myString.toUpperCase());
+console.log(myString.substring(12, duljina));
+
+var myNiz = myString.split(' ');
+
+console.log('kraj');
