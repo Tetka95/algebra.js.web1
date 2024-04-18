7ab832d2a43eda96ab55489e903af30cc4903239
tetka95 <martina.bobek95@gmail.com>
2024-04-18 18:43:31 +0200

    dodan komentar

---
+++ skripte/sestiprimjer.js
@@ -0,0 +1,7 @@
+var x = 0;
+/// prvo ispiši vrijednost pa onda povećaj varijablu za 1
+console.log(x++);
+console.log(x);
+/// prvo povećaj varijablu za 1 pa onda ispiši vrijednost
+console.log(x++)
+console.log(x);
