f1048723f3891885d9f37fdba7df69ce242d1865
Tetka95 <martina.bobek95@gmail.com>
2024-04-22 16:45:29 +0200

    ponavljanje

---
+++ skripte/osmiprimjer.js
@@ -0,0 +1,21 @@
+var x = 'Lorem ipsum',
+    y = 2345,
+    z = '2345',
+    q = false;
+
+var trueFalse = !!false;
+console.log('1. ' + trueFalse);
+
+console.log(q || x || y || z);
+
+var num = 6;
+num--;
+console.log(num);
+
+const price = 26.5;
+const taxRate = 0.082;
+
+let totalPrice = price + price * taxRate
+totalPrice = totalPrice.toFixed(2)
+
+console.log('Total:', totalPrice);
