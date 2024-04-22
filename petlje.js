b8f8036a817cc915b32ed0115a4ed2de4480169f
Tetka95 <martina.bobek95@gmail.com>
2024-04-22 17:57:34 +0200

    drugi zadatak

--- petlje.js
+++ petlje.js
@@ -1,3 +1,4 @@
+//1. zadatak
 var x = 0
 while (x < 20) {
     x++;
@@ -17,3 +18,12 @@ while (x < 20) {
     }
     console.log('x=' + x); 
 }
+
+//2. zadatak
+var varOne = 0;
+for (i = 0; i < 4; i++) {
+    for  (j = 0; j < 4; j++) {
+        varOne++;
+    }
+}
+console.log(varOne);
