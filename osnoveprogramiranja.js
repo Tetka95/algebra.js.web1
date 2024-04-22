5291461f4336c7b43dfebcb27e2eff6c2690964a
Tetka95 <martina.bobek95@gmail.com>
2024-04-22 18:19:54 +0200

    zadatci 1 i 2

---
+++ osnoveprogramiranja.js
@@ -0,0 +1,19 @@
+//1. zadatak
+var firstName = "Ivan";
+var lastName = "Horvat";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+//2. zadatak
+var z= 6;
+var y = 0;
+
+var x = z === 2 ? y : 5;
+
+if (z === 2) {
+    x = y;
+} else {
+    x = 5;
+}
+console.log('x = ' + x);
