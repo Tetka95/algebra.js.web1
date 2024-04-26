7e957a87fcb6c4a44e93b29631c2e44d920efe7d
Tetka95 <martina.bobek95@gmail.com>
2024-04-26 17:01:08 +0200

    return

--- skripte/funkcije.js
+++ skripte/funkcije.js
@@ -1,5 +1,12 @@
 function ispisi(unos) {
-    console.log('---> ' + unos);
+    console.log('DEBUG: ---> ' + unos);
+    var x = 2;
+    x++;
+    return x;
 }
 
-ispisi('nešto ispiši na konzolu');
+ispisi('nešto ispiši na konzolu'); //samo ispis, bez vrijednosti
+
+var nekaVarijabla = ispisi(); //kako bi pokazali da funkcija nema vrijednost
+
+console.log('>>>' + nekaVarijabla); //vratit će vrijednost zbog returna
