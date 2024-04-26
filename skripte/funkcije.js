7f017b77494fa841b7daf99c3568264f7f708d36
Tetka95 <martina.bobek95@gmail.com>
2024-04-26 17:18:15 +0200

    zadatak

--- skripte/funkcije.js
+++ skripte/funkcije.js
@@ -1,3 +1,4 @@
+//1.
 function ispisi(unos) {
     console.log('DEBUG: ---> ' + unos);
     var x = 2;
@@ -10,3 +11,22 @@ ispisi('nešto ispiši na konzolu'); //samo ispis, bez vrijednosti
 var nekaVarijabla = ispisi(); //kako bi pokazali da funkcija nema vrijednost
 
 console.log('>>>' + nekaVarijabla); //vratit će vrijednost zbog returna
+
+
+//2.
+var nekiNiz = ['ime', 'prezime', 'godine', 'broj cipela'];
+
+function pronadjiNajduzuRijec(niz) {
+    var najduzaRijec = '';
+    var duljina = 0;
+    for(var element of nekiNiz) {
+        if (element.length > duljina) {
+            duljina = element.length;
+            najduzaRijec = element;
+        }
+    }
+    return najduzaRijec;
+}
+
+var najduzaRijec = pronadjiNajduzuRijec(nekiNiz);
+console.log(najduzaRijec);
